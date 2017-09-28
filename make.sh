9fcd644f05949118d17f0e5641e840099b4b4bea
Chez Sick <chezsick@bbox.fr>
2017-09-28 15:35:47 +0200

    No Execute After Compiled And Remove .out

--- make.sh
+++ make.sh
@@ -1,2 +1,1 @@
-g++ Graham.cpp BigInt.cpp FFTMul.cpp KaratsubaMul.cpp -std=c++11 -pthread -O2 -o graham.out -march=core-avx2
-./graham.out
+g++ Graham.cpp BigInt.cpp FFTMul.cpp KaratsubaMul.cpp -std=c++11 -pthread -O2 -o graham -march=core-avx2
